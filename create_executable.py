5a92a6daa366008e11a49c4c586a26abba98aaac
Nathan Lam <nathancy@hawaii.edu>
2018-10-25 14:17:25 -1000

    Simple create executable script with file argument

--- create_executable.py
+++ create_executable.py
@@ -1,14 +1,25 @@
+#!c:\python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'PyInstaller==3.4','console_scripts','pyinstaller'
+
+# Usage: python create_executable.py
+# Usage: python create_executable.py -f <file.py>
 # Manual: python pyinstaller-script.py --onefile --windowed <file.py>
 
 from os import system
 import argparse
+__requires__ = 'PyInstaller==3.4'
+import re
+import sys
+from pkg_resources import load_entry_point
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", required=False, help="File name to convert into executable")
 args = vars(ap.parse_args())
 if not args['file']:
-    filename = 'SpectrumGUI'
+    filename = 'spectrumGUI.py'
 else:
     filename = args['file']
-system('python executable/pyinstaller-script.py --onefile --windowed ' + filename)
+
+system('python pyinstaller-script.py --onefile --windowed ' + filename)
+
 
