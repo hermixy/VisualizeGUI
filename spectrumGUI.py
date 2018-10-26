a00b74dce3992f6d9e7a4ae7e61fcc3ed5707c1b
Nathan Lam <nathancy@hawaii.edu>
2018-10-26 10:16:54 -1000

    Initial GUI setup and menubar addition

--- spectrumGUI.py
+++ spectrumGUI.py
@@ -2,17 +2,50 @@
 # coding: utf-8
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+import pyqtgraph as pg
 
 class Window(QtGui.QMainWindow):
 
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle("PyQT tuts!")
+        self.setWindowTitle("GUI")
         self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+
+        self.initMenuBar()
+        #self.initToolBar()
+
+        self.statusBar()
+
+        # Show the widgets onto the GUI, must be at the end
         self.show()
 
-app = QtGui.QApplication(sys.argv)
-GUI = Window()
-sys.exit(app.exec_())
+    # ------------------------------------------------------------------------
+    # GUI Setup
+    # ------------------------------------------------------------------------
+    def initMenuBar(self):
+        self.menuBar = self.menuBar()
+        self.fileMenu = self.menuBar.addMenu('&File')
+        # Close program
+        self.exitAction = QtGui.QAction('Exit', self)
+        self.exitAction.setShortcut('Ctrl+Q')
+        self.exitAction.setStatusTip('Exit application')
+        self.exitAction.triggered.connect(QtGui.qApp.quit)
+        self.fileMenu.addAction(self.exitAction)
+    def initToolBar(self):
+        self.toolBar = self.addToolBar('Exit')
+        self.toolBar.addAction(self.exitAction)
+
+    # ------------------------------------------------------------------------
+    # Menu Option functions
+    # ------------------------------------------------------------------------
+
+
+def run():
+    app = QtGui.QApplication([])
+    GUI = Window()
+    sys.exit(app.exec_())
+
+run()
+
