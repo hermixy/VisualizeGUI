5c719a67ba3b822928d57802ec4d397a648a44c4
Nathan Lam <nathancy@hawaii.edu>
2018-10-31 11:24:39 -1000

    pyqtgraph example file

---
+++ example.py
@@ -0,0 +1,8 @@
+# pyqtgraph includes an extensive set of examples that can be accessed by running this script
+# This script will start a launcher with a list of available examples. Select an item from
+# the list to view its source code and double click an item to run the example
+#
+# Usage: python example.py
+
+import pyqtgraph.examples
+pyqtgraph.examples.run()
