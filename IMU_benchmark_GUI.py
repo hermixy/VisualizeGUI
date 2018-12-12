10004dd7a523f0d17cc5586f7436f9362ea3d87c
Nathan Lam <nathancy@hawaii.edu>
2018-12-12 11:34:35 -1000

    In progress dynamic add/remove IMUs using .ini file

--- IMU_benchmark_GUI.py
+++ IMU_benchmark_GUI.py
@@ -5,25 +5,72 @@ import numpy as np
 import zmq
 import sys
 import time
+import configparser
+
+def readSettings():
+    global IMU_list
+
+    try:
+        config = configparser.ConfigParser()
+        config.read('IMU.ini')
+        # Pub/Sub 192.168.1.133:6010 10000
+        reference_address = str(config['REFERENCE']['referenceAddress'])
+        reference_topic = str(config['REFERENCE']['referenceTopic'])
+        reference_sleep_frequency = float(config['REFERENCE']['sleepFrequency'])
+        reference_timer_frequency = reference_sleep_frequency * 1000
+       
+        IMU_list = []
+        IMU_list.append((reference_address, reference_topic, reference_sleep_frequency, reference_timer_frequency))
+        numberOfIMUs = int(config['IMU']['numberOfIMUs'])
+        if numberOfIMUs > 0:
+            for imu in range(1, numberOfIMUs + 1):
+                IMU_address = str(config['IMU']['IMU' + str(imu) + 'Address'])
+                IMU_topic = str(config['IMU']['IMU' + str(imu) + 'Topic'])
+                # time.sleep (s)
+                IMU_sleep_frequency = float(config['IMU']['IMU' + str(imu) + 'SleepFrequency'])
+                # timer.timer (ms)
+                IMU_timer_frequency = IMU_sleep_frequency * 1000
+                IMU_list.append((IMU_address, IMU_topic, IMU_sleep_frequency, IMU_timer_frequency))
+    except KeyError:
+        print('File doesnt exist')
+        exit(1)
 
 def initZMQHandshake():
-    global reference_socket
-    global IMU1_socket
+    global IMU_list
+    global IMU_sockets
+    global IMU_position
+    global IMU_timestamp 
+    global IMU_topic
+    global IMU_plot_buffer
+    global IMU_plot_data
+    
+    try:
+        IMU_sockets = []
+        IMU_position = []
+        IMU_timestamp = []
+        IMU_topic = []
+        IMU_plot_buffer = []
+        IMU_plot_data = []
+        for imu in range(len(IMU_list)):
+            context = zmq.Context()
+            socket = context.socket(zmq.SUB)
+            socket.connect(IMU_list[imu][0])
+            socket.setsockopt(zmq.SUBSCRIBE, IMU_list[imu][1])
+            topic, timestamp, position = socket.recv().split()
+            IMU_topic.append(topic)
+            IMU_timestamp.append(timestamp)
+            IMU_position.append(position)
+            IMU_sockets.append(socket)
 
-    reference_context = zmq.Context()
-    reference_socket = reference_context.socket(zmq.SUB)
-    reference_socket.connect(reference_address)
-    reference_socket.setsockopt(zmq.SUBSCRIBE, reference_topic)
-    topic, reference_timestamp, reference_position = reference_socket.recv().split()
+    except:
+        print("no socket")
 
-    IMU1_context = zmq.Context()
-    IMU1_socket = IMU1_context.socket(zmq.SUB)
-    IMU1_socket.connect(IMU1_address)
-    IMU1_socket.setsockopt(zmq.SUBSCRIBE, IMU1_topic)
-    topic, IMU1_timestamp, IMU1_position = IMU1_socket.recv().split()
-    IMU2_position = -1 * IMU1_position
-
-def readReferenceDataThread():
+def plotBufferInit():
+    reference_plot_buffer = int((abs(left_x) + abs(right_x))/sleep_frequency)
+    reference_plot_data = []
+    IMU1_plot_buffer = int((abs(left_x) + abs(right_x))/sleep_frequency)
+    IMU1_plot_data = []
+def readDataThread():
     global reference_socket
     global reference_timestamp 
     global reference_position 
@@ -40,13 +87,11 @@ def readIMU1DataThread():
     global IMU1_socket
     global IMU1_timestamp 
     global IMU1_position 
-    global IMU2_position 
     global sleep_frequency
     
     while True:
         try:
             topic, IMU1_timestamp, IMU1_position = IMU1_socket.recv().split()
-            IMU2_position = float(IMU1_position) * -1
         except:
             print('error')
         time.sleep(sleep_frequency)
@@ -61,32 +106,24 @@ l = QtGui.QGridLayout()
 mw.setCentralWidget(cw)
 cw.setLayout(l)
 
-# Pub/Sub 192.168.3.133:6010 10000
-reference_address = "tcp://192.168.1.133:6010"
-reference_topic = "10000"
-
-# Pub/Sub 192.168.3.133:6011 10000
-IMU1_address = "tcp://192.168.1.133:6011"
-IMU1_topic = "10000"
-
-# time.sleep (s)
-sleep_frequency = .02 
-
-# timer.timer (ms)
-timer_frequency = sleep_frequency * 1000
-
+readSettings()
 initZMQHandshake()
+print(IMU_list)
+
+print IMU_sockets
+print IMU_position
+print IMU_timestamp 
+print IMU_topic
+exit(1)
 
 # Initial graph ranges and view spacing (right_x has to be 0)
 left_x = -15
 right_x = 0
-x_axis = np.arange(left_x, right_x, sleep_frequency)
-reference_plot_buffer = int((abs(left_x) + abs(right_x))/sleep_frequency)
+x_axis = np.arange(left_x, right_x, IMU_list[0][3])
+keference_plot_buffer = int((abs(left_x) + abs(right_x))/sleep_frequency)
 reference_plot_data = []
 IMU1_plot_buffer = int((abs(left_x) + abs(right_x))/sleep_frequency)
 IMU1_plot_data = []
-IMU2_plot_buffer = int((abs(left_x) + abs(right_x))/sleep_frequency)
-IMU2_plot_data = []
 
 plot = pg.PlotWidget()
 plot.addLegend()
@@ -97,20 +134,17 @@ plot.setLabel('bottom', 'Timestamp')
 
 reference_plot = plot.plot(name="Reference")
 IMU1_plot = plot.plot(name="IMU1")
-IMU2_plot = plot.plot(name="IMU2")
 l.addWidget(plot, 1, 0, 1, 6)
 
 reference_plot.setPen('w', width=2)
 IMU1_plot.setPen('r', width=2)
-IMU2_plot.setPen(26,170,85, width=2)
 
 def update():
     global reference_timestamp, IMU1_timestamp 
     global reference_position, IMU1_position 
     global reference_plot_buffer, reference_plot_data
-    global reference_plot, IMU1, IMU2
+    global reference_plot, IMU1
     global IMU1_plot_buffer, IMU1_plot_data
-    global IMU2_plot_buffer, IMU2_plot_data
     global x_axis, plot, count, left_x, right_x
     global inverted_plot
 
@@ -120,7 +154,6 @@ def update():
     # Controls proper data movement
     reference_plot.setPos(right_x, 0)
     IMU1_plot.setPos(right_x, 0)
-    IMU2_plot.setPos(right_x, 0)
 
     # Controls axis shifting
     plot.setXRange(left_x, right_x)
@@ -130,17 +163,12 @@ def update():
         reference_plot_data.pop(0)
     if len(IMU1_plot_data) >= IMU1_plot_buffer:
         IMU1_plot_data.pop(0)
-    if len(IMU2_plot_data) >= IMU2_plot_buffer:
-        IMU2_plot_data.pop(0)
     
     reference_plot_data.append(float(reference_position))
     reference_plot.setData(x_axis[len(x_axis) - len(reference_plot_data):], reference_plot_data)
 
     IMU1_plot_data.append(float(IMU1_position))
     IMU1_plot.setData(x_axis[len(x_axis) - len(IMU1_plot_data):], IMU1_plot_data)
-
-    IMU2_plot_data.append(float(IMU2_position))
-    IMU2_plot.setData(x_axis[len(x_axis) - len(IMU2_plot_data):], IMU2_plot_data)
 
 readReferenceDataThread = Thread(target=readReferenceDataThread, args=())
 readReferenceDataThread.daemon = True
