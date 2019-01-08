6895d04abedf872cb1d2639300803f4fa088db1e
Nathan Lam <nathancy@hawaii.edu>
2019-01-07 17:08:11 -1000

    Video stream widget

--- widgets.py
+++ widgets.py
@@ -345,60 +345,98 @@ class PlotColorWidget(QtGui.QColorDialog):
         self.palette = self.getColor()
         plotObject.changePlotColor(str(self.palette.name()))
 
-class VideoDisplayWidget(QtGui.QWidget):
+class VideoStreamWidget(QtGui.QWidget):
     def __init__(self, parent=None):
-        super(VideoDisplayWidget, self).__init__(parent)
-
+        super(VideoStreamWidget, self).__init__(parent)
+        
         self.capture = None
         self.videoFileName = None
-        self.isVideoFileLoaded = False
-
-        self.layout = QtGui.QFormLayout()
+        self.isVideoFileOrStreamLoaded = False
+        self.buttonSize = 75
+        self.pause = True
+        self.image_width = 400
+        self.image_height = 400
+        
+        self.layout = QtGui.QGridLayout()
         self.loadButton = QtGui.QPushButton('Select Video')
         self.loadButton.clicked.connect(self.loadVideoFile)
-        self.loadButton.setFixedWidth(50)
+        self.loadButton.setFixedWidth(self.buttonSize)
         self.startButton = QtGui.QPushButton('Start')
         self.startButton.clicked.connect(self.startCapture)
-        self.startButton.setFixedWidth(50)
+        self.startButton.setFixedWidth(self.buttonSize)
         self.pauseButton = QtGui.QPushButton('Pause')
         self.pauseButton.clicked.connect(self.pauseCapture)
-        self.pauseButton.setFixedWidth(50)
+        self.pauseButton.setFixedWidth(self.buttonSize)
 
-        #self.layout.addRow(self.loadButton, self.startButton, self.pauseButton)
-        self.layout.addRow(self.loadButton)
-        self.layout.addRow(self.startButton)
-        self.layout.addRow(self.pauseButton)
+        self.buttonLayout = QtGui.QHBoxLayout()
+        self.buttonLayout.addWidget(self.loadButton)
+        self.buttonLayout.addWidget(self.startButton)
+        self.buttonLayout.addWidget(self.pauseButton)
+
+        self.layout.addLayout(self.buttonLayout,0,0)
 
         self.videoFrame = QtGui.QLabel()
-        self.layout.addWidget(self.videoFrame)
+        self.layout.addWidget(self.videoFrame,1,0)
+        
+        self.placeholder_image = cv2.imread('placeholder.PNG')
 
+        self.placeholder_image = imutils.resize(self.placeholder_image, width=min(self.image_width, self.placeholder_image.shape[1]))
+        #self.placeholder_image = imutils.resize(self.placeholder_image, width=self.image_width, height=self.image_height)
+        #self.placeholder_image = cv2.resize(self.placeholder_image, (self.image_width, self.image_height))
+        self.placeholder_image = QtGui.QImage(self.placeholder_image, self.placeholder_image.shape[1], self.placeholder_image.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+        self.placeholder_image = QtGui.QPixmap.fromImage(self.placeholder_image)
+        self.videoFrame.setPixmap(self.placeholder_image)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.getFrame)
     def getVideoDisplayLayout(self):
         return self.layout
-
-    def startCapture(self):
-        if not self.capture:
-            self.capture = cv2.VideoCapture(str(self.videoFileName))
-        else:
-            self.start()
-
+    
     def loadVideoFile(self):
         try:
             self.videoFileName = QtGui.QFileDialog.getOpenFileName(self, 'Select .h264 Video File')
-            self.isVideoFileLoaded = True
+            if self.videoFileName:
+                self.isVideoFileOrStreamLoaded = True
+                self.pause = False
+                self.capture = cv2.VideoCapture(str(self.videoFileName))
         except:
             print("Please select a .h264 file")
 
-    def nextFrameSlot(self):
-        status, frame = self.capture.read()
-        img = QtGui.QImage(frame, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
-        pix = QtGui.QPixmap.fromImage(img)
-        self.videoFrame.setPixmap(pix)
+    def startCapture(self):
+        if self.isVideoFileOrStreamLoaded:
+            self.pause = False
+            self.timer.start(10)
 
-    def start(self):
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.nextFrameSlot)
-        self.timer.start(.1)
+    def getFrame(self):
+        if not self.pause and self.capture.isOpened():
+            status, frame = self.capture.read()
+            if frame is not None:
+                frame = imutils.resize(frame, width=min(self.image_width, frame.shape[1]))
+                #frame = imutils.resize(frame, width=self.image_width, height=self.image_height)
+                #frame = cv2.resize(frame, (self.image_width, self.image_height))
+                img = QtGui.QImage(frame, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+                pix = QtGui.QPixmap.fromImage(img)
+                self.videoFrame.setPixmap(pix)
+            # Reached end of file so stop timer and pause video
+            else:
+                self.pauseCapture()
 
     def pauseCapture(self):
-        self.timer.stop()
+        if not self.pause:
+            self.pause = True
+            self.timer.stop()
 
+    def verifyNetworkStream(self, link):
+        cap = cv2.VideoCapture(link)
+        if cap is None or not cap.isOpened():
+            print('Warning: unable to open video link')
+            return False
+        return True
+
+    def openNetworkStream(self):
+        text, okPressed = QtGui.QInputDialog.getText(self, "Open Media", "Please enter a network URL:")
+        link = str(text)
+        if okPressed and self.verifyNetworkStream(link):
+            self.isVideoFileOrStreamLoaded = True
+            self.pause = False
+            self.capture = cv2.VideoCapture(link)
