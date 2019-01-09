eb8343df52223b78c22911b4cc264f0c469f7e38
Nathan Lam <nathancy@hawaii.edu>
2019-01-08 16:28:59 -1000

    Crosshair plot widget

--- widgets.py
+++ widgets.py
@@ -440,3 +440,70 @@ class VideoStreamWidget(QtGui.QWidget):
             self.isVideoFileOrStreamLoaded = True
             self.pause = False
             self.capture = cv2.VideoCapture(link)
+
+class CrosshairPlotWidget(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(CrosshairPlotWidget, self).__init__(parent)
+        
+        # Use for time.sleep (s)
+        self.frequency = .025
+        # Use for timer.timer (ms)
+        self.timer_frequency = self.frequency * 1000
+
+        self.left_x = -10
+        self.right_x = 0
+        self.x_axis = np.arange(self.left_x, self.right_x, self.frequency)
+        self.buffer = int((abs(self.left_x) + abs(self.right_x))/self.frequency)
+        self.data = []
+        
+        self.crosshairPlot = pg.PlotWidget()
+        self.crosshairPlot.setXRange(self.left_x, self.right_x)
+        self.crosshairPlot.setLabel('left', 'Value')
+        self.crosshairPlot.setLabel('bottom', 'Time (s)')
+        self.crosshairColor = (196,220,255)
+
+        self.plot = self.crosshairPlot.plot()
+
+        self.layout = QtGui.QGridLayout()
+        self.layout.addWidget(self.crosshairPlot)
+        
+        self.crosshairPlot.plotItem.setAutoVisible(y=True)
+        self.vLine = pg.InfiniteLine(angle=90)
+        self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.vLine.setPen(self.crosshairColor)
+        self.hLine.setPen(self.crosshairColor)
+        self.crosshairPlot.setAutoVisible(y=True)
+        self.crosshairPlot.addItem(self.vLine, ignoreBounds=True)
+        self.crosshairPlot.addItem(self.hLine, ignoreBounds=True)
+        
+        self.crosshairUpdate = pg.SignalProxy(self.crosshairPlot.scene().sigMouseMoved, rateLimit=60, slot=self.updateCrosshair)
+
+        self.start()
+
+    def plotUpdater(self):
+        self.dataPoint = random.randint(1,101)
+        if len(self.data) >= self.buffer:
+            self.data.pop(0)
+        self.data.append(float(self.dataPoint))
+        self.plot.setData(self.x_axis[len(self.x_axis) - len(self.data):], self.data)
+
+    def updateCrosshair(self, event):
+        coordinates = event[0]  
+        if self.crosshairPlot.sceneBoundingRect().contains(coordinates):
+            mousePoint = self.crosshairPlot.plotItem.vb.mapSceneToView(coordinates)
+            index = mousePoint.x()
+            if index > self.left_x and index <= self.right_x:
+                self.crosshairPlot.setTitle("<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y=%0.1f</span>" % (mousePoint.x(), mousePoint.y()))
+            self.vLine.setPos(mousePoint.x())
+            self.hLine.setPos(mousePoint.y())
+
+    def start(self):
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.plotUpdater)
+        self.timer.start(self.getTimerFrequency())
+
+    def getCrosshairPlotLayout(self):
+        return self.layout
+
+    def getTimerFrequency(self):
+        return self.timer_frequency
