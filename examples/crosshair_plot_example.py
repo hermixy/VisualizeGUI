0103b21ff6f37b8e4585841ae3864d701dde1cc8
Nathan Lam <nathancy@hawaii.edu>
2019-01-16 11:55:45 -1000

    Crosshair and scrolling plot examples

--- examples/crosshair_plot_example.py
+++ examples/crosshair_plot_example.py
@@ -1,18 +1,21 @@
-import numpy as np
-import pyqtgraph as pg
 from PyQt4 import QtCore, QtGui
+import pyqtgraph as pg
+import numpy as np
 import random
+import sys
 
-# Example to create a PlotWidget graph with a crosshair
+# Scrolling plot widget with crosshair, adjustable X-axis, and dynamic Y-axis 
 class CrosshairPlotWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         super(CrosshairPlotWidget, self).__init__(parent)
         
         # Use for time.sleep (s)
         self.frequency = .025
+
         # Use for timer.timer (ms)
         self.timer_frequency = self.frequency * 1000
 
+        # Set X Axis range. If desired is [-10,0] then set LEFT_X = -10 and RIGHT_X = 0
         self.left_x = -10
         self.right_x = 0
         self.x_axis = np.arange(self.left_x, self.right_x, self.frequency)
@@ -21,18 +24,15 @@ class CrosshairPlotWidget(QtGui.QWidget):
        
         # Create the plot
         self.crosshairPlot = pg.PlotWidget()
+        self.crosshairPlot.plotItem.setMouseEnabled(x=False, y=False)
         self.crosshairPlot.setXRange(self.left_x, self.right_x)
         self.crosshairPlot.setLabel('left', 'Value')
         self.crosshairPlot.setLabel('bottom', 'Time (s)')
-        self.crosshairColor = (196,220,255)
 
         self.plot = self.crosshairPlot.plot()
 
-        self.layout = QtGui.QGridLayout()
-        self.layout.addWidget(self.crosshairPlot)
-       
         # Create the crosshair
-        self.crosshairPlot.plotItem.setAutoVisible(y=True)
+        self.crosshairColor = (196,220,255)
         self.vLine = pg.InfiniteLine(angle=90)
         self.hLine = pg.InfiniteLine(angle=0, movable=False)
         self.vLine.setPen(self.crosshairColor)
@@ -41,6 +41,9 @@ class CrosshairPlotWidget(QtGui.QWidget):
         self.crosshairPlot.addItem(self.vLine, ignoreBounds=True)
         self.crosshairPlot.addItem(self.hLine, ignoreBounds=True)
         
+        self.layout = QtGui.QGridLayout()
+        self.layout.addWidget(self.crosshairPlot)
+       
         # Update crosshair
         self.crosshairUpdate = pg.SignalProxy(self.crosshairPlot.scene().sigMouseMoved, rateLimit=60, slot=self.updateCrosshair)
         
@@ -50,6 +53,7 @@ class CrosshairPlotWidget(QtGui.QWidget):
     def plotUpdater(self):
         # Change for desired data
         self.dataPoint = random.randint(1,101)
+
         if len(self.data) >= self.buffer:
             self.data.pop(0)
         self.data.append(float(self.dataPoint))
@@ -76,13 +80,17 @@ class CrosshairPlotWidget(QtGui.QWidget):
     def getTimerFrequency(self):
         return self.timer_frequency
 
+# Create main application window
 app = QtGui.QApplication([])
 app.setStyle(QtGui.QStyleFactory.create("Cleanlooks"))
 mw = QtGui.QMainWindow()
-mw.setWindowTitle('Crosshair plot')
+mw.setWindowTitle('Crosshair Plot Example')
 
+# Create crosshair plot
 crosshairPlot = CrosshairPlotWidget()
 
+# Create and set widget layout
+# Main widget container
 cw = QtGui.QWidget()
 ml = QtGui.QGridLayout()
 cw.setLayout(ml)
