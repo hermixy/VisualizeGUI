6f38989371c9fa00e5081df89ac3ab742b9858d5
Nathan Lam <nathancy@hawaii.edu>
2019-02-20 10:35:58 -1000

    Progress bar example CSS

--- examples/progressbar_example.py
+++ examples/progressbar_example.py
@@ -1,6 +1,34 @@
 from PyQt4 import QtGui, QtCore
 import sys
 
+progressbar_style = """
+    QProgressBar {
+	border: 2px solid grey;
+	border-radius: 5px;
+	text-align: center;
+    }
+
+    QProgressBar::chunk{
+        background-color: #05B8CC;
+    }
+"""
+pushbutton_style = """
+    QPushButton {
+	border: 2px solid #8f8f91;
+	border-radius: 6px;
+	background-color: #5d84ae;
+	min-width: 80px;
+    }
+
+    QPushButton:pressed {
+	background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,stop: 0 #dadbde, stop: 1 #f6f7fa);
+    }
+
+    QPushButton:default {
+	border-color: navy; /* make the default button prominent */
+    }
+"""
+
 """Progressbar Widget Example"""
 
 class ProgessbarWidget(QtGui.QWidget):
@@ -9,8 +37,10 @@ class ProgessbarWidget(QtGui.QWidget):
         
         self.progressbar = QtGui.QProgressBar(self)
         self.progressbar.setGeometry(30, 40, 200, 25)
+        self.progressbar.setStyleSheet(progressbar_style)
 
         self.button = QtGui.QPushButton('Start', self)
+	self.button.setStyleSheet(pushbutton_style)
         self.button.clicked.connect(self.toggle_progressbar)
 
         self.timer = QtCore.QBasicTimer()
@@ -36,30 +66,31 @@ class ProgessbarWidget(QtGui.QWidget):
         else:
             self.timer.start(50,self)
             self.button.setText('Stop')
+
     def get_progressbar_widget_layout(self):
         return self.layout
 
-# Create main application window
-app = QtGui.QApplication([])
-app.setStyle(QtGui.QStyleFactory.create("Cleanlooks"))
-mw = QtGui.QMainWindow()
-mw.setWindowTitle('Progressbar Example')
-
-# Create and set widget layout
-# Main widget container
-cw = QtGui.QWidget()
-ml = QtGui.QGridLayout()
-cw.setLayout(ml)
-mw.setCentralWidget(cw)
-
-# Create progressbar Widget
-progressbar = ProgessbarWidget()
-
-ml.addLayout(progressbar.get_progressbar_widget_layout(),0,0)
-
-mw.show()
-
 if __name__ == '__main__':
+    # Create main application window
+    app = QtGui.QApplication([])
+    app.setStyle(QtGui.QStyleFactory.create("Cleanlooks"))
+    mw = QtGui.QMainWindow()
+    mw.setWindowTitle('Progressbar Example')
+
+    # Create and set widget layout
+    # Main widget container
+    cw = QtGui.QWidget()
+    ml = QtGui.QGridLayout()
+    cw.setLayout(ml)
+    mw.setCentralWidget(cw)
+
+    # Create progressbar Widget
+    progressbar = ProgessbarWidget()
+
+    ml.addLayout(progressbar.get_progressbar_widget_layout(),0,0)
+
+    mw.show()
+
     if(sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
 
